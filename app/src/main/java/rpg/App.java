ca562cb2c82d78e537f5ff47a0cd9bfe187fa250
Elim Garak <garak92@protonmail.com>
2024-08-22 00:54:12 -0400

    Quantum multithreading

--- app/src/main/java/rpg/App.java
+++ app/src/main/java/rpg/App.java
@@ -7,7 +7,6 @@ import static javafx.application.Application.launch;
 
 public class App {
   public static void main(String[] args) {
-    System.setProperty("quantum.multithreaded", "false");
     launch(Game.class, args);
   }
 
